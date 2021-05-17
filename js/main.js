4ac888df0166ab3c350237dea079907b081e6b78
john <c.ioan12@yahoo.com>
2021-05-17 14:46:17 +0200

    asd

--- js/main.js
+++ js/main.js
@@ -1,97 +1,269 @@
 jQuery(function($) {
-	function success(position) {
-		var pageOverlay = jQuery('.page-overlay');
 
-		pageOverlay.find('h3').text('Retrieving weather information...')
+	var $weatherInfoWrap = jQuery('.weather-info-wrap'),
 
-		var lat = position.coords.latitude,
-			lon = position.coords.longitude;
+		$weatherInfoOverlay = $weatherInfoWrap.find('.weather-info-overlay'),
+		$weatherInfoOverlayText = $weatherInfoOverlay.find('h3'),
 
-		$.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lon + '&sensor=false&key=AIzaSyByUe5jHZIiNa9TnDOQKmDIy8EcRvHa92I', function(locInfo) {
-			// Convert unix time into day of the week
-			function time(timestamp) {
-				var a = new Date(timestamp*1000);
-				var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-				return days[a.getDay()];
-			}
+		// current weather
+		$currWeatherWrap = $('.weather-info-current'),
+		$currWeatherLocationWrap = $('.wic-location'),
+		$currWeatherLocationCity = $currWeatherLocationWrap.find('.wic-location-city'),
+		$currWeatherLocationCountry = $currWeatherLocationWrap.find('.wic-location-country'),
+		$currWeatherTemp = $currWeatherWrap.find('.wic-temp h1'),
+		$currWeatherState = $('.wif-current-state'),
+		$currWeatherStateIcon = $currWeatherState.find('.wif-icon img'),
+		$currWeatherStateDescr = $currWeatherState.find('.wif-description h4'),
 
-			function upper(text) {
-				return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-			}
+		// lat and lon coordinates
+		lat, lon,
 
-
-			// Decide temperature format (metric/imperial)
-			var impCountryCodes = ['BS', 'BZ', 'KY', 'PW', 'US'],
-				units;
-			if(impCountryCodes.indexOf(locInfo.results[0].address_components[5].short_name) == -1) units='metric';
-				else units = 'imperial';
-			function unitText() {
-				if (units == 'metric') return ' &deg;C'; else return ' &deg;F';
-			}
-
-			$('.location h2').html(locInfo.results[0].address_components[2].long_name);
-			$('.location h3').html(locInfo.results[0].address_components[5].long_name);
+		// list of imperial countries and units
+		impCountryCodes = ['BS', 'BZ', 'KY', 'PW', 'US'],
+		units;
 
 
-			// Get information about current and forecast weather
-			$.ajax({
-				'async': true,
-				'crossDomain': true,
-				'url': 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat +'&lon=' + lon + '&appid=0e4ac4b36b6cdcf2661160019d9f5154&exclude=minutely,hourly,alerts&units=' + units,
-				'method': 'GET',
-				success: function (weatherInfo) {
-					console.log(weatherInfo);
 
-					// Display weather information
-					var currentWeather = weatherInfo.current,
-						currWeatherCondition = currentWeather.weather[0],
-						currWeatherConditionDescr = upper(currWeatherCondition.description),
-						currWeatherConditionCode = currWeatherCondition.icon,
-						currentWeatherTemp = currentWeather.temp;
+	// On page load, try to get the weather info
+	// for the user's current location
+	getCurrentLocationWeather();
 
 
-					$('.curr-wthr .container').css('background-image', 'url("images/bgs/' + currWeatherConditionCode + '.jpg")');
-					$('.curr-wthr .temp h1').html(Math.ceil(currentWeatherTemp) + '<span>' + unitText() + '</span>');
 
-					$('.curr-wthr-state .icon img').attr('src', 'images/icons/' + currWeatherConditionCode + '.svg');
-					$('.curr-wthr-state .description').html(currWeatherConditionDescr);
+	// If the user chooses so, try to get the weather info
+	// for the user's current location
+	var $currentLocationBtn = $('.current-location');
 
+	$currentLocationBtn.on('click', function () {
 
-					// Display forecast weather information
-					var forecastWeather = weatherInfo.daily,
-						currForecastWeather,
-						currForecastWeatherConditionCode,
-						currForecastWeatherTempMin,
-						currForecastWeatherTempMax,
-						i = 1;
-
-					$('.daily .day').each(function(day) {
-						currForecastWeather = forecastWeather[i];
-
-						currForecastWeatherConditionCode = currForecastWeather.weather[0].icon;
-
-						currForecastWeatherTempMin = Math.ceil(currForecastWeather.temp.min);
-						currForecastWeatherTempMax = Math.ceil(currForecastWeather.temp.max);
+		// Reset the location input
+		$locationInput.val('');
 
 
-						$(this).children('.heading').html(time(currForecastWeather.dt));
-						$(this).children('.icon').children('img').attr('src', 'images/icons/' + currForecastWeatherConditionCode + '.svg');
-						$(this).children('.temp').html(currForecastWeatherTempMin + ' / ' + currForecastWeatherTempMax + unitText());
+		// Display the overlay background and reset the overlay text
+		$weatherInfoWrap.removeClass('weather-info-loaded-2');
+		$weatherInfoOverlayText.text('Retrieving weather information...');
 
-						i++;
+
+		setTimeout(function () {
+			// Display the overlay text
+			$weatherInfoWrap.removeClass('weather-info-loaded-1');
+
+			// Try to get the weather info for the
+			// user's current location
+			setTimeout(function () {
+				getCurrentLocationWeather();
+			}, 500);
+		}, 250);
+
+	});
+
+
+
+	// Try to get the weather info for the location
+	// the user is searching for
+	var $locationForm = $('#locationForm'),
+		$locationInput = $locationForm.find('#location'),
+		address = '';
+
+	$locationForm.on('submit', function (e) {
+
+		e.preventDefault();
+
+		var locationInputVal = $locationInput.val();
+
+		// If the location input is not empty, try to get the
+		// weather info for the location the user is searching for
+		if( locationInputVal !== '' ) {
+			address = locationInputVal;
+
+			$weatherInfoWrap.removeClass('weather-info-loaded-2');
+			$weatherInfoOverlayText.text('Retrieving weather information...');
+
+			setTimeout(function () {
+				$weatherInfoWrap.removeClass('weather-info-loaded-1');
+
+				setTimeout(function () {
+					$.ajax({
+						'async': true,
+						'crossDomain': true,
+						'url': 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyByUe5jHZIiNa9TnDOQKmDIy8EcRvHa92I',
+						'method': 'GET',
+						success: function (locationInfo) {
+
+							// Decide temperature format (metric/imperial)
+							// based on country code
+							if (impCountryCodes.indexOf(locationInfo.results[0].address_components[3].short_name) == -1)
+								units='metric';
+							else units = 'imperial';
+
+
+							// Display the city and country names
+							$currWeatherLocationCity.html(locationInfo.results[0].address_components[0].long_name);
+							$currWeatherLocationCountry.html(locationInfo.results[0].address_components[3].long_name);
+
+
+							// Get and display weather info
+							lat = locationInfo.results[0].geometry.location.lat;
+							lon = locationInfo.results[0].geometry.location.lng;
+
+							getWeatherInfo(lat, lon);
+
+						}
 					});
+				}, 500);
+			}, 250);
+		}
+		// If the location input is empty, try to get the
+		// weather info for the user's current location
+		else {
+			getCurrentLocationWeather();
+		}
 
-					setTimeout(function () {
-						pageOverlay.addClass('hidden-content');
+	});
 
-						setTimeout(function () {
-							pageOverlay.addClass('hidden-overlay');
-						}, 400);
-					}, 300);
-				}
-			});
-		});
+
+
+	// Function to the the weather info for
+	// the user's current location
+	function getCurrentLocationWeather() {
+		navigator.geolocation.getCurrentPosition(
+			// The location is turned on
+			function (position) {
+
+				// Store the latitude and longitude coordinates
+				lat = position.coords.latitude;
+				lon = position.coords.longitude;
+
+				$.ajax({
+					'async': true,
+					'crossDomain': true,
+					'url': 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lon + '&sensor=false&key=AIzaSyByUe5jHZIiNa9TnDOQKmDIy8EcRvHa92I',
+					'method': 'GET',
+					success: function (locationInfo) {
+
+						// Decide temperature format (metric/imperial)
+						// based on country code
+						if (impCountryCodes.indexOf(locationInfo.results[0].address_components[5].short_name) == -1)
+							units='metric';
+						else units = 'imperial';
+
+
+						// Display the city and country names
+						$currWeatherLocationCity.html(locationInfo.results[0].address_components[2].long_name);
+						$currWeatherLocationCountry.html(locationInfo.results[0].address_components[5].long_name);
+
+
+						// Get and display weather info
+						getWeatherInfo(lat, lon);
+
+					}
+				});
+
+			},
+
+			// The location is turned off
+			function () {
+
+				$weatherInfoOverlayText.html('Unable to retrieve your location.<br>Please activate your location setting, then refresh the page.');
+
+			}
+		)
 	}
 
-	navigator.geolocation.getCurrentPosition(success);
+
+
+
+	// Function to get weather information based on
+	// lat and lon coordinates
+	function getWeatherInfo(lat, lon) {
+
+		$.ajax({
+			'async': true,
+			'crossDomain': true,
+			'url': 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat +'&lon=' + lon + '&appid=0e4ac4b36b6cdcf2661160019d9f5154&exclude=minutely,hourly,alerts&units=' + units,
+			'method': 'GET',
+			success: function (weatherInfo) {
+
+				// Display current weather information
+				var currentWeather = weatherInfo.current,
+					currWeatherCondition = currentWeather.weather[0],
+					currWeatherConditionDescr = upper(currWeatherCondition.description),
+					currWeatherConditionCode = currWeatherCondition.icon,
+					currentWeatherTemp = currentWeather.temp;
+
+				// Deal with the current weather background
+				$currWeatherWrap.css('background-image', 'url("images/bgs/' + currWeatherConditionCode + '.jpg")');
+
+				// Deal with the current weather temperature
+				$currWeatherTemp.html(Math.ceil(currentWeatherTemp) + '<span>' + unitText() + '</span>');
+
+				// Deal with the current weather icon
+				$currWeatherStateIcon.attr('src', 'images/icons/' + currWeatherConditionCode + '.svg');
+
+				// Deal with the current weather condition
+				$currWeatherStateDescr.html(currWeatherConditionDescr);
+
+
+				// Display forecast weather information
+				var forecastWeather = weatherInfo.daily,
+					currForecastWeather,
+					currForecastWeatherConditionCode,
+					currForecastWeatherTempMin,
+					currForecastWeatherTempMax,
+					i = 1;
+
+				$('.wif-daily .wif-day').each(function(day) {
+					currForecastWeather = forecastWeather[i];
+
+					// Deal with the forecast day name
+					$(this).children('.wif-heading').html(time(currForecastWeather.dt));
+
+					// Deal with the forecast icon
+					$(this).children('.wif-icon').children('img').attr('src', 'images/icons/' + currForecastWeather.weather[0].icon + '.svg');
+
+					// Deal with the forecast temperature
+					$(this).children('.wif-temp').html(Math.ceil(currForecastWeather.temp.min) + ' / ' + Math.ceil(currForecastWeather.temp.max) + unitText());
+
+					i++;
+				});
+
+
+				// Remove the overlay
+				setTimeout(function () {
+					$weatherInfoWrap.addClass('weather-info-loaded-1');
+
+					setTimeout(function () {
+						$weatherInfoWrap.addClass('weather-info-loaded-2');
+					}, 450);
+				}, 400);
+			}
+		});
+
+	}
+
+
+
+	// Function to convert unix time into day of the week
+	function time(timestamp) {
+		var a = new Date(timestamp*1000);
+		var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+		return days[a.getDay()];
+	}
+
+
+
+	// Function to decide which unit of meaasurement text
+	// to display based on unit measurement type
+	function unitText() {
+		if (units === 'metric') return ' &deg;C'; else return ' &deg;F';
+	}
+
+
+
+	// Function to transform first letter of string to uppercase
+	function upper(text) {
+		return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+	}
+
 });
